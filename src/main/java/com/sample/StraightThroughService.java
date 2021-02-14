5e3a98252c86b82dcdff94519e3ac0f28ef1bf66
Karina Varela <kmacedovarela@gmail.com>
2021-02-14 12:55:56 -0300

    first commit

---
+++ src/main/java/com/sample/StraightThroughService.java
@@ -0,0 +1,42 @@
+package com.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jbpm.workflow.instance.impl.WorkflowProcessInstanceImpl;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class StraightThroughService {
+	
+	private KieSession ksession;
+	
+	public StraightThroughService() {
+		 KieServices kieServices = KieServices.Factory.get();
+		 KieContainer kcontainer = kieServices.getKieClasspathContainer();
+		 ksession = kcontainer.getKieBase("kbase").newKieSession();
+	}
+	
+	public Map<String, Object> startProcess(String processId, Map<String, Object> params) {
+		ksession.getWorkItemManager().registerWorkItemHandler("Service", new WorkItemHandler() {
+			public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+//				System.out.println("Execution workItem " + workItem);
+				try {
+					Thread.sleep(500);
+				} catch (Throwable t) {
+					// do nothing
+				}
+				manager.completeWorkItem(workItem.getId(), new HashMap<String, Object>());
+			}
+			public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+			}
+		});
+		WorkflowProcessInstanceImpl p = (WorkflowProcessInstanceImpl) ksession.startProcess("sample-process", params);
+		return p.getVariables();
+	}
+
+}
---
+++ src/test/java/com/sample/StraightThroughTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sample;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.jbpm.process.instance.impl.demo.SystemOutWorkItemHandler;
+import org.junit.Test;
+
+public class StraightThroughTest {
+
+    @Test()
+    public void testStraightThroughProcess() throws Exception {
+    	StraightThroughService service = new StraightThroughService();
+    	Runnable r = new Runnable() {
+			public void run() {
+    			int counter = 0;
+    			while (counter < 10) {
+    				service.startProcess("sample-process", Collections.emptyMap());
+    				counter++;
+    			}
+			}
+
+		};
+
+		long start = System.currentTimeMillis();
+		List<Thread> ts = new ArrayList<Thread>();
+		for (int i = 0; i < 10; i++) { 
+			Thread t = new Thread(r);
+			t.start();
+			ts.add(t);
+		}
+		for (Thread t: ts) {
+			t.join();
+		}
+		System.out.println(System.currentTimeMillis() - start);
+    }
+    
+}
